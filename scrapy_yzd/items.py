16264c610db0cb6988c8529f3623b755ff699e68
qqxx6661 <404013419@qq.com>
2016-09-12 19:53:46 +0800

    教程用例还没实现好

--- scrapy_yzd/items.py
+++ scrapy_yzd/items.py
@@ -11,6 +11,9 @@ import scrapy
 class ScrapyYzdItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    pass
+
+class DmozItem(scrapy.Item):
     title = scrapy.Field()
     link = scrapy.Field()
     desc = scrapy.Field()
--- scrapy_yzd/spiders/dmoz_spider.py
+++ scrapy_yzd/spiders/dmoz_spider.py
@@ -1,14 +1,23 @@
 import scrapy
+from scrapy.selector import Selector
+from scrapy_yzd.items import DmozItem
 
-class DmozSpider(scrapy.spiders.Spider):
+class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/"
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        sel = Selector(response)
+        sites = sel.xpath('//div[@class="site-title"]/')
+        items = []
+
+        for site in sites:
+            item = DmozItem()
+            item['title'] = site.xpath('text()').extract()
+            #item['url'] = site.xpath('a/@href').extract()
+            #item['description'] = site.xpath('text()').re('-\s[^\n]*\\r')
+            items.append(item)
+        return items
